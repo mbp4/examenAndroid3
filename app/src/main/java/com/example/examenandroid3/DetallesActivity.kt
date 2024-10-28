8610a9e6794834865e829cc9dfc339bbee55588b
mbp4 <mblanpon@myuax.com>
2024-10-28 17:57:21 +0100

    layouts unidos

--- app/src/main/java/com/example/examenandroid3/DetallesActivity.kt
+++ app/src/main/java/com/example/examenandroid3/DetallesActivity.kt
@@ -1,7 +1,13 @@
 package com.example.examenandroid3
 
+import android.os.Bundle
 import androidx.activity.ComponentActivity
 
 class DetallesActivity: ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalles)
+    }
 
 }
--- app/src/main/java/com/example/examenandroid3/ListadoActivity.kt
+++ app/src/main/java/com/example/examenandroid3/ListadoActivity.kt
@@ -1,6 +1,12 @@
 package com.example.examenandroid3
 
+import android.os.Bundle
 import androidx.activity.ComponentActivity
 
 class ListadoActivity: ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listado)
+    }
 }
